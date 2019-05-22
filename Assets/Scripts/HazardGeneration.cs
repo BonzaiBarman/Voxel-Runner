7df8afea0131d558aa40588426ac0b69a55a4629
BonzaiBarman <bonzaibarman@gmail.com>
2019-05-21 23:32:18 -0600

    May 21 Home

---
+++ Assets/Scripts/HazardGeneration.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HazardGeneration : MonoBehaviour
+{
+    
+	public GameObject[] hazards;
+	
+	public float timeBetweenHazards;
+	private float hazardGenCounter;
+	
+	// Start is called before the first frame update
+    void Start()
+    {
+	    hazardGenCounter = timeBetweenHazards;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+	    hazardGenCounter -= Time.deltaTime;
+	    
+	    if(hazardGenCounter <= 0)
+	    {
+		    int chosenHazard = Random.Range(0, hazards.Length);
+		    Instantiate(hazards[chosenHazard], transform.position, Quaternion.Euler(0f, Random.Range(-45f, 45f), 0f));
+		    hazardGenCounter = Random.Range(timeBetweenHazards * 0.75f, timeBetweenHazards * 1.25f);
+	    }
+
+    }
+}
